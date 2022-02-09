162cf76f466fe035c69b7e5f76218b82eda8f50b
Tasmiya Kankurti <tasmiyakankurti@FVFGW2NHQ05N.local>
2022-02-08 21:29:16 -0800

    Removed unused packages

--- src/routes/fileRoute.js
+++ src/routes/fileRoute.js
@@ -2,19 +2,17 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const imageUpload = require('../middleware/upload')
-const readXlsxFile = require("read-excel-file/node");
 
-router.post('/uploadBulkImage', imageUpload.array('excel', 2), (req, res) => {
-    res.send(req.files)
- }, (error, req, res, next) => {
-     res.status(400).send({ error: error.message })
- })
-
-// For Single image upload
 router.post('/uploadfile', imageUpload.single('excel'), (req, res) => {
     res.send(req.file)
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
+
+router.post('/uploadBulkfiles', imageUpload.array('excel', 2), (req, res) => {
+    res.send(req.files)
+ }, (error, req, res, next) => {
+     res.status(400).send({ error: error.message })
+ })
 
 module.exports = router
